30b25b4c175df9f006e389bac5dc9cfef285460e
Tarun-1104 <tarunpoopathi@outlook.com>
2024-06-24 19:29:23 +0530

    Add files via upload

---
+++ Arithmetic Operations.py
@@ -0,0 +1,21 @@
+#Addition
+num1 = 20
+num2 = 5
+sum_result = num1 + num2
+print("Addition:", sum_result)  # Output: Addition: 25
+#Subtraction
+num1 = 20
+num2 = 5
+sub_result = num1 - num2
+print("Subtraction:", sub_result)  # Output: Subtraction: 15
+#Multiplication
+num1 = 20
+num2 = 5
+mul_result = num1 * num2
+print("Multiplication:", mul_result)  # Output: Multiplication: 100
+#division
+num1 = 20
+num2 = 5
+div_result = num1 / num2
+print("Division:", div_result)  # Output: Division: 4.0
+
